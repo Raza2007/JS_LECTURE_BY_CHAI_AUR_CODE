1b7803aab92dbb5c45da6b5ad28d35c2579b2bbe
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-30 17:58:42 +0000

    Intro_to_datatypes

---
+++ JS_FULL_DETAILS/Third.js
@@ -0,0 +1,85 @@
+
+// Introducations to DataTypes:
+
+"use strict";  // treat all JS code as newer versions
+
+
+
+// console.log(12+12)
+// console.log("Umar")
+
+let name ="Umar"
+let age =18
+let isLoggedIn =false
+// let state;
+
+let state="Maharastra"
+// console.log([name,age])
+console.table([name,age,state])
+
+
+
+/*
+
+Primitive Datatypes:
+
+// DataTypes in javaScript
+// (i) Number
+// (ii) String
+// (iii)Boolean
+// (iv)bigint
+// (v) Null
+
+
+
+*/
+// Object:
+
+
+console.log(typeof "umar"); // string
+console.log(typeof null); // object
+
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
