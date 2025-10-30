3db4522f29e63c937ca9eba6092cc32794918795
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-30 18:45:59 +0000

    Why string to number conversion is confusing:

---
+++ JS_FULL_DETAILS/Fifth.js
@@ -0,0 +1,42 @@
+// Why string to number conversion is confusing:
+
+// ---------------->Operations:---------------
+
+let value =12
+let negValue =-value
+
+// console.log(negValue);
+// console.log(-value);
+
+console.log(12+12);
+console.log(12-12);
+console.log(12*12);
+console.log(12/12);
+console.log(12**12);
+console.log(12%12);
+
+let str1 ="Mr_Umar"
+let str2 =" Raza"
+let str3=str1+str2;
+console.log(str3);
+
+
+console.log("1"+"2")
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2"+2)
+
+
+console.log(3+22-12*1)
+
+
+// special types of conversions:
+
+console.log(+true);
+console.log(+false);
+
+let num1 ,num2,num3
+num1 =num2=num3=12+12
+console.log(num2);
+
+
