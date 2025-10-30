0e02be7824ebd20940fdfecc7100110d32793203
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-30 18:18:42 +0000

    Datatype conversion confusion

---
+++ JS_FULL_DETAILS/Four.js
@@ -0,0 +1,54 @@
+
+// Datatype conversion confusion:
+
+// let score =19
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+
+let score ="19"
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let age = 12
+console.log(typeof age);
+
+// let score ="12abc"
+// let score= null
+// let score = undefined
+// let score=false
+
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber );
+console.log(valueInNumber);
+
+
+let someNumber = 22
+let stringNumber =string(someNumber)
+console.log(stringNumber);
+
+
+
+
+/*
+
+"99"=> 99
+"0777ahmad" =>NaN
+true => 1;
+false => 0;
+
+
+
+
+
+
+
+
+
+
+
+*/
