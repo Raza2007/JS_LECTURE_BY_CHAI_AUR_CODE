99169c2e3dd7c2a5ed31d52e1f5b76c2759a50dd
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-30 17:36:35 +0000

    Informations of let var and const

---
+++ JS_FULL_DETAILS/second.js
@@ -0,0 +1,28 @@
+            // ----> Introductions to let,cont,var <--------
+
+// How to declare a variable:
+
+// const acccount_Id = 083373
+
+// constent ko declare kerne ke liye const keyword ka use kiya jata hai
+
+// variable ko declare karne ke liye do methode hai let and var
+// var ko jada use nahi kiya jata hai 
+
+let account_Email = "umarahmad512@gmail.com"
+var account_Password ="122222"
+
+account_City = "Gonda"
+
+acccount_Id =22
+account_Email ="log512@gmail.com"
+account_Password ="84884"
+account_City ="Aazamgarh"
+let account_state;
+// let account_state="uk"
+
+// console.log(acccount_Id);
+
+// console.log([acccount_Id,account_City,account_Email,account_Password]);
+
+console.table([acccount_Id,account_City,account_Email,account_Password,account_state]);
